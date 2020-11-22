28ab6851a9a4370f809a775774a108a89432e04a
Erkin Pardayev <erkin.pardayev@gmail.com>
2020-11-22 17:41:52 +0500

    Solved p10

---
+++ projecteuler.net/p10.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ *
+ */
+
+class p10
+{
+  public function solve() : int
+  {
+    $summ = 0;
+    for ($i = 2; $i <= 2000000; $i++) {
+      if ($this->isPrime($i)) {
+        $summ = $summ + $i;
+      }
+    }
+    return $summ;
+  }
+  public function isPrime($num) {
+
+    if ($num == 2) {
+      return true;
+    }
+
+    if($num % 2 == 0) {
+      return false;
+    }
+
+    $ceil = ceil(sqrt($num));
+    for($i = 3; $i <= $ceil; $i = $i + 2) {
+      if($num % $i == 0)
+        return false;
+    }
+    return true;
+  }
+}
+
+$p10 = new p10();
+echo $p10->solve();
