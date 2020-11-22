0b7c7a3d798d5c2da381cdde63c9ef6858d72962
Erkin Pardayev <erkin.pardayev@gmail.com>
2020-11-22 17:42:37 +0500

    fix p10

--- projecteuler.net/p10.php
+++ projecteuler.net/p10.php
@@ -21,10 +21,6 @@ class p10
       return true;
     }
 
-    if($num % 2 == 0) {
-      return false;
-    }
-
     $ceil = ceil(sqrt($num));
     for($i = 3; $i <= $ceil; $i = $i + 2) {
       if($num % $i == 0)
